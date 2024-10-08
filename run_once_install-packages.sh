75c4df4885c4104105a8a0cb1b852c7168d40cd2
Afzal M <afzaalm059@gmail.com>
2024-10-08 08:57:25 +0100

    add homebrew pack list

---
+++ run_once_install-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install homebrew if not installed
+if ! command -v brew &> /dev/null; then
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install packages
+brew bundle --no-lock --file=~/.local/share/chezmoi/Brewfile
