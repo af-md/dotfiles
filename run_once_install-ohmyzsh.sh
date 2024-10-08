0d508c9806772869a7a58f414c502da088cc8207
Afzal M <afzaalm059@gmail.com>
2024-10-08 09:58:22 +0100

    add oh my zsh

---
+++ run_once_install-ohmyzsh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install oh-my-zsh if not installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh already installed"
+fi
